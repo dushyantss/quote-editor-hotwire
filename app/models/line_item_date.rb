dd11112a0feba9e0797c6a47458106d43a533c92
Dushyant Singh Shekhawat <contact@dushyantss.com>
2022-11-11 22:54:04 +0530

    Add LineItem

--- app/models/line_item_date.rb
+++ app/models/line_item_date.rb
@@ -4,6 +4,7 @@ class LineItemDate < ApplicationRecord
   scope :ordered, -> { order(date: :asc) }
 
   belongs_to :quote
+  has_many :line_items, dependent: :destroy
 
   validates :date, presence: true, uniqueness: { scope: :quote_id }
 
